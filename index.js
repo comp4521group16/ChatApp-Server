71d9dbf7ba3cb21199e2db089668125ff4f95a14
wnwong <tsubasa10wangngai@gmail.com>
2016-04-08 22:49:52 +0800

    send image success

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var user = {};
+
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html');
 })
