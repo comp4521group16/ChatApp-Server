23d53a9fec1e80585754905bbfa80f692775a604
wnwong <tsubasa10wangngai@gmail.com>
2016-05-19 20:02:28 +0800

    Check added to ensure both users are in the same chatroom before emitting messages

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var pushbots = require('./pushbots');
 var user = {};
+var chatWith = {};
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -12,17 +13,20 @@ io.on('connection', function (socket) {
 
     socket.on('join', function (data) { //Every time the user connect to the socket, map his username with the socket he connected to
         user[data.username] = socket;
+        chatWith[data.username] = data.receiver; // To ensure both users are iin the same chatroom
         console.log("Saved username: " + data.username);
         console.log("Saved socket: " + user[data.username].id);
+        console.log(data.username + " is chatting with: " + data.receiver);
     })
 
     socket.on('message', function (data) {
         console.log("Data received from the socket: " + data);
-        if (user[data.receiver] != null) { // If the receiver is now connected to the socket, send him the message
+        console.log("chatWith[data.receiver]: "+chatWith[data.receiver]);
+        if (user[data.receiver] != null && chatWith[data.receiver]==data.sender) { // If the receiver is now connected to the socket, send him the message
             user[data.receiver].emit('message', {message: data.text, image: data.image});
             console.log("Receiver: " + data.receiver);
             console.log("No notification needed");
-        }else if(user[data.receiver] == null) {
+        }else {
             console.log("Calling Notification", data.receiver, data.text);
             pushbots.send(data);
         }
@@ -31,6 +35,7 @@ io.on('connection', function (socket) {
 
     socket.on('offline', function (data) { // When the user is not connected to the socket, set his socket to null
         user[data.username] = null;
+        chatWith[data.username] = null;
         console.log("Deleted username: " + data.username);
         console.log("Deleted socket_id: " + user[data.username]);
     })
